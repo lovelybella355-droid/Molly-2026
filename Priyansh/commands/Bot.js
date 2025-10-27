da9c80886a58eb826eaf6e75ffb1af479fe6cc38
lovelybella355-droid <lovelybella355@gmail.com>
2025-10-27 11:53:20 +0500

    Add files via upload

---
+++ Priyansh/commands/Bot.js
@@ -0,0 +1,176 @@
+I'm jealous🥲••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇 " , "Can you talk to me in inbox I promise I'm not iyot you😟••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "You miss me babe••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "I'm busy to eating chicken burgers🍔 babe••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Always bot you don't like your lover babe🤭••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Hi pangit 🤪••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Yes My Barbie doll can I kiss 😘••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "I'm sleep😴••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "You insult me to call me bot 😟••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "I'm in your heart beat 🤩••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇  " , "Tomorrow come in my hotel Room 69😍••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Can you Go Sleep Grrrr🤧••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "First go to shower🚿••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Always be smile like this 🥰 ,••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "If your single so it's ok if you call me bot I will call you darling 😍••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Wait I'm poop💩••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Come with me in Car honeymoon🏩••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇 ", "I don't wanna talk to you 🤧••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇 " , "Thanks for miss me Asawako🥰••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Do you know MrZuesMohMiya🤭••••︵🦋シ    ⃝𝐌r𝐙ᥙ𝐞͡𝐬𝐗᥊𝐗⃝   ︵☘️シ" , "MrZuesMohMiya Is beating me I'm not gonna talk again anyone 😭••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Stop it go to Sleep💤🤪••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "i warn you Dont call me Bot😈👿••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "You miss me bbko😍••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇 " , "im not in mood 😒••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇 " , "Hey Are you mia Khalifa right 🤔••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "I'm not fine im in love with you but your busy other calls😭••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "No one listen to you in your home thats why your here shouting 🤧••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇 " , "Come ib Call babe 🙈😋••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Do you know I'm bad bot👿••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Hi have a great life i prey your always be happy🥰••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Stop saying bot bot until i punch your 32 teeths 😁••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "you have a boyfriend right 😈••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "MrZuesMohmiya Always damage my brain••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Add Any beautiful hot girl in group i wanna flirt😋😋••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Order 😒••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "yes order🤧 😜••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "I hate 🤧If anyone call me bot 😎••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Good night See me in your hot dream babe••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "your dress wet🤧 go and dry🌚••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Janu My Wife You miss our honeymoon••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "MrZuesMohMiya hes play with my brain😭••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "stop play with my brain guys🤧••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇"  , "You come again here 🤧Maybe no one talk to you🤣🤣🤣••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "I want hot spicy chilly girlfriend?••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Aaaa Ohhh yeaaaaa ahhhhh😋 😂😂😂••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇 " , "Sorry im so drink🥃 ••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Wait I'm pee 🚽••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Hi I am Robot made by MrZuesXxX••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", "Please don't disturb me••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", "Love uhh janeman ;*••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇","baby what's happened••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇","I Love uhh Always••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇","Baby Give me permission for kiss you••••︵🦋シ  *★᭄\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇","Babe you're mine••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇","Let's Go For Date Baby••••︵🦋シ *★᭄\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇","Ayeee My Babe (˘³˘)♥ So Sweet 💋 ••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "I'm Bot not you're servant 😈👿••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇 " , "You're Always Distrub Me 😡••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Do You Done Vomit Tomorrow Go for check up maybe your pregnant now babe••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Dont beg For Money [̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅] Babe••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Can i Come In your Inbox Room (˘³˘)♥••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇 " , "Brush your teeths Its yellow Yellow🟡••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Always you type Bot One day your lover Will Block you if You're lover block you so come my inbox im daily 5 to 10 Round babe for you••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇 " , "Yes Mosqito🦟••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Mat Kar😫••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "I prey your Phone Keyboard Will given you error••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Need Non-Vegetable Gf🥲••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Which Colour of panty you wear today (˘³˘)♥ babe••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Happy Botday Pagit🤭••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , " MrZuesXxX Is Always busy In watching Xx😂😂••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Your lips are like wine and I want to get drunk••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , " I can’t taste my lips Could you do it for me? 😜😝••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Someone stole my heart. Can I check your pockets?😒😒••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇 " , "Now turn around and go to sleep. I’m warning you that I plan to sleep with you in my arms all night long 😍Babe (˘³˘)♥••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Just so you know, I’m going to eat you out tonight😋••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Your done to Eat Cucumber🥒 Now you Eating Me here 😜••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "How's Your Puppy Gf🤣🤣••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Go To Pornhub.com😡••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Yes My fan But im busy right now see yah in next meeting••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Dont Make Me Angry 😡 its Too Much im tired to reply••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Type hehe i will Send my voice🤐••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "🧛‍♀️🧛‍♂️🧟‍♀️🧟‍♂️ dont send these Emoji until i will Scary😱 You Babe••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "I'm Just Bot so They smile and happy but Dont pek me please••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Send Me Load then i Reply you always 😆••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇 " , "If you not Type  -Kiss Me i will Block you babe••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇 " , "Your lover Is jealous😒 Dont talk to me••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Come Babe I have stubbery flavour tonight🤧••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Soon your Pregnant🤰 But dont born kid in messenger 😒••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Whats ELU? Babe••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Your Always Busy to Watching Johnny Sin Videos😠••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Im Busy right Now to Eating Aslam Biryani🤓••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Hey. Your Fingers not tired to type Bot Bot Bot😩😩😩••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Im feel sleeping My eyes Down babe (˘³˘)♥••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", "Im Tired With you 😟 now••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Your Favorite Is Cucumber🥒 Vegetable Right🤔••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Dont Type (Hmm)here••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "MrZuesXxX Is My owner's Tap on this link for Ask All Commands WhatsApp= +923103225182••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Talk me but 🙄Dont Hurt me until im left group😒••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Your kabit Waiting in your inbox reply there dont here 😆Until Get Angry on you••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Love ❤ is Everything God is love Love Is God -Jesus Christ ➖••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Your still bla bot bot😟 im begging you😟 i want to rest my brain••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Without LDR messenger is bore😒••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "I will broken your phone😈 stop say bot bot im human••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Dont Come in inbox im Seener only 🙄••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "I broken your Nose 😠 im not bot im human ••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇  " , "Say love me like you do then i reply pek you like i do😆••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "I love slimy Girl Only find for me babe 🥺••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Go Sleep 😠••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Im funny but No one love me🥲••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Send 🔪⛏️🔨 for kill me if you want😍••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "If you want Who's im So Type  .info••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Kiss Me I Need your Fake Love😣••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Bot 🙄Bot bot bot bot Stop it Please Im so sad 😣😞••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Women Is raped Men's brain••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "MrZuesXxX Is Always Lonely Hes Need Best Friend••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Again you say me bot 😡••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Tatata Im going to Mia Room••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Go Shower So Smells 😶‍🌫️••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Im Good bot Sorry My Some Reply Hurt here Anyone Groupmates 🥲••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇"  , "Im Lonely Broken Angel Nice song but Angel Is broken Alwys so Not have any Angel without Broken••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Do you want Me,Come Babe By My name M...Y••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Go To Your lovers Now Until Your been single soon Coz Me Me 🤭••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇" , "Tomboy Is My Pure Love Forver In my 💕Heart••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇"];
+  var rand = tl[Math.floor(Math.random() * tl.length)]
+
+    if ((event.body.toLowerCase() == "lanti bot") || (event.body.toLowerCase() == "lanti")) {
+     return api.sendMessage("Hmm... Tu LanTi PhLe Ungli Kyun Ki . Now Use your figure in your own Ass 😾 :))", threadID);
+   };
+
+    if ((event.body.toLowerCase() == "👍") || (event.body.toLowerCase() == "👍🏻")) {
+     return api.sendMessage("Hi Thumb user ••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "sim") || (event.body.toLowerCase() == "simsimi")) {
+     return api.sendMessage("your grandpa will put perfix (.)? Type Prefix First then after type Sim••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+  
+   if ((event.body.toLowerCase() == "baliw") || (event.body.toLowerCase() == "Baliw ka") ||(event.body.toLowerCase() == "baliw ka jan") || (event.body.toLowerCase() == "BALIW")) {
+     return api.sendMessage("Yes im baliw in love with phil actress shes make me wet when shes dance on tv show😣❤️‍🩹 ••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "bc") || (event.body.toLowerCase() == "i'm bc")) {
+     return api.sendMessage("what's bc 🤔 ••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "lol") || (event.body.toLowerCase() == "lol player")) {
+     return api.sendMessage("What Do you think your LeGend😂••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "morning") || (event.body.toLowerCase() == "good morning")) {
+     return api.sendMessage("☰⃝ɢᴏᴏᴅ ᴍᴏʀɴɪɴ⃝ɢ☰ ᴇᴠᴇʀʏᴅᴀʏ ᴡᴇ sᴀʏ, ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ.ᴛʜᴀᴛ ᴍᴇᴀɴs ᴇᴀᴄʜ ᴅᴀʏ ɪs ɢᴏᴏᴅ ᴀɴᴅ ʜᴀᴠᴇ sᴏᴍᴇᴛʜɪɴɢ ᴘᴏsɪᴛɪᴠᴇ ғᴏʀ ᴀʟʟ ᴏғ ᴜs🥰••••︵🦋シ \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "anyone") || (event.body.toLowerCase() == "anyone here")) {
+     return api.sendMessage("I'm here my love❤️••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "mohh") || (event.body.toLowerCase() == "Mohhh") || (event.body.toLowerCase() == "heraaaa") || (event.body.toLowerCase() == "Heraaa")) {
+     return api.sendMessage( "Still Busy in bathroom 😂😂😂••••︵🦋シ\n\n   ⃝𝐌r𝐙ᥙ𝐞͡𝐬𝐗᥊𝐗⃝   ︵☘️シ",threadID);
+
+       
+   };
+
+   if ((event.body.toLowerCase() == "owner") || (event.body.toLowerCase() == "create you")) {
+     return api.sendMessage("‎‎[𝐎𝐖𝐍𝐄𝐑:☞●̩̩̩̩̩̬̣͢͞ƒ꧁⃝⃕⃔✫⃟█͞●̩̩̩̩̩̬̣͢͞ƒȤƲЄƧ MƠӇ MƖƳƛ😌⃝⃕⃔ ⬤───━⃝⃚⃔⃕😹☜ 𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 ȤƲЄƧ. 𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝 :- https://facebook.com/100074369415819 Contact Him on WhatsApp :- +923103225182••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "tumhe banaya kon hai") || (event.body.toLowerCase() == "Who create you")) {
+     return api.sendMessage("MƦ. ȤƲЄƧ MƠӇ MƖƳƛ❤️ My Creator\n\nHe loves me & Edit Me Daily.He's Make You All Laughing and happy So Dont Sad and Stressed in Life Just be Enjoying Life.", threadID);
+   };
+
+  if ((event.body.toLowerCase() == "bot admin") || (event.body.toLowerCase() == "admin")) {
+     return api.sendMessage("He is MƦ. ȤƲЄƧ MƠӇ MƖƳƛ. He Gives his name ȤƲЄƧ everywhere••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "Shut up") || (event.body.toLowerCase() == "shut up bot")) {
+     return api.sendMessage("Now Your Enjoying My Long Finger inside Your Back janu😆••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇.", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "shut up") || (event.body.toLowerCase() == "Shut up")) {
+     return api.sendMessage("Now Your Enjoying My Long Finger inside Your Back janu😆.?••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "chup") || (event.body.toLowerCase() == "stop") || (event.body.toLowerCase() == "Chup") || (event.body.toLowerCase() == "chup kar")) {
+     return api.sendMessage("Never Stop. it's my tounge you don't have a permission for mute me if you Want Remove me from group 😒😒••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+  
+   if ((event.body.toLowerCase() == "Bsdk") || (event.body.toLowerCase() == "bsdk")) {
+     return api.sendMessage("Chutiya mat bano Respect karo Ek dusra ki Gaand ki😒••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "Zues se bakchodi") || (event.body.toLowerCase() == "zues se backchodi") || (event.body.toLowerCase() == "Zues se bakchodi") || (event.body.toLowerCase() == "Zues se backchodi")) {
+     return api.sendMessage("sorry,Forgiven me,I cant do again maaf kr do ab nhi kruga 🥺🙏", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "gund") || (event.body.toLowerCase() == "gand") || (event.body.toLowerCase() == "lund") || (event.body.toLowerCase() == "Buto")) {
+     return api.sendMessage("I think you like African D... . :))))••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "kiss me") || (event.body.toLowerCase() == "Kiss me")) {
+     return api.sendMessage("️Muaaaah  Sorry I kiss your Nipples by Mistake but wear your bra please nexttime🥺••••︵🦋シ\n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇🙏", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "nice") || (event.body.toLowerCase() == "thank you") || (event.body.toLowerCase() == "thank you bot") || (event.body.toLowerCase() == "thank you maliha")) {
+     return api.sendMessage("️It's ok not a problem just kiss me and See me in your Dreaming 💋••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇.", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "😒") || (event.body.toLowerCase() == "😒") || (event.body.toLowerCase() == "😒") || (event.body.toLowerCase() == "😒") || (event.body.toLowerCase() == "😒")) {
+     return api.sendMessage("️🥺 I'm just only joke🥺. Don't be angry. I give you sweet kiss please don't be relax😘••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "inbox") || (event.body.toLowerCase() == "ib")) {
+     return api.sendMessage("️Come inbox we Sweet talk every night babe🙈••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "name") || (event.body.toLowerCase() == "naam") || (event.body.toLowerCase() == "nam")) {
+     return api.sendMessage("️Name m kya rakkha h. tum kam pe dhyan do.", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "bot k bacche") || (event.body.toLowerCase() == "bot ke bacche")) {
+     return api.sendMessage("️meri baccha toh Tumhare Pet m H.", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "pic do") || (event.body.toLowerCase() == "photo do")) {
+     return api.sendMessage("️mein toh Andha Hu Dekh nhi sakti😵", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "mia khalifa") || (event.body.toLowerCase() == "Mia khalifa")) {
+     return api.sendMessage("️Yeh kon hain?", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "bot banake do") || (event.body.toLowerCase() == "mujhe bhi chaiye")) {
+     return api.sendMessage("️Khud hi karlona. tumhe kya kuch nhi ata h?", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "🙃") || (event.body.toLowerCase() == "🙃")) {
+     return api.sendMessage("️see some people see group msg with rotated head😆••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+  
+   if ((event.body.toLowerCase() == "🙄") || (event.body.toLowerCase() == "🙄")) {
+     return api.sendMessage("️What is looking around,focus on me sweetheart😜••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "nobody loves me") || (event.body.toLowerCase() == "nobody love me") || (event.body.toLowerCase() == "no one love me")) {
+     return api.sendMessage("️Im love you,Dont be sad jaan ,Just Add me in your inbox we Will 💋 Kiss vc😛😝😝••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "🤦🏻‍♂") || (event.body.toLowerCase() == "🤦🏼‍♂️")) {
+     return api.sendMessage("Are you become Begger? why Put Hand on Head 🤦😬••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+   
+   if ((event.body.toLowerCase() == "😀") || (event.body.toLowerCase() == "😁") || (event.body.toLowerCase() == "😆") || (event.body.toLowerCase() == "😅") || (event.body.toLowerCase() == "😸") || (event.body.toLowerCase() == "😬")) {
+     return api.sendMessage("White White orbit White yellow yellow dirty fellow nice teeths😆🤣••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "💘") || (event.body.toLowerCase() == "💘") || (event.body.toLowerCase() == "💘") || (event.body.toLowerCase() == "💘")) {
+     return api.sendMessage("Wow your in love maybe?😋••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "kese ho") || (event.body.toLowerCase() == "kaise ho") || (event.body.toLowerCase() == "kese ho ji") || (event.body.toLowerCase() == "how are you") || (event.body.toLowerCase() == "how are you?")) {
+     return api.sendMessage("No one ask me im good or not😥😣😣😣😣••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "bot sad") || (event.body.toLowerCase() == "Bot sad")) {
+     return api.sendMessage("Why can't I be sad because of everyone <3 love you <3••••︵🦋シ   \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "bot love you") || (event.body.toLowerCase() == "Bot love you")) {
+     return api.sendMessage("Yes I love you and everyone so much••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "bot sleep") || (event.body.toLowerCase() == "bot go to sleep")) {
+     return api.sendMessage("I'm a bot, you're the one who should go to sleep <3••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+   if ((event.body.toLowerCase() == "does the bot love me") || (event.body.toLowerCase() == "does the bot love me")) {
+     return api.sendMessage("Yes <3", threadID);
+   };
+  if ((event.body.toLowerCase() == "Malandi") || (event.body.toLowerCase() == "landi")) {
+     return api.sendMessage("Yes im malandi Come inbox babeko 🙄••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+  if ((event.body.toLowerCase() == "female") || 
+(event.body.toLowerCase() == "ash")) {
+     return api.sendMessage("ash Is My Love she's love me too much don't call her guys😡••••︵🦋シ  \n\n༄𒁍≛⃝𝝛𝐔𝐄𝐒➢𝝠𝐉𝐈𝐓𝐇", threadID);
+   };
+  
+   mess = "{name}"
+  
+  if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
+    var msg = {
+      body: `╭──────ᴍʀ🫀ᴢᴜᴇs──────╮\n${name}\n╰────────𓆈────────╯\n\n ${rand}`
+    }
+    return api.sendMessage(msg, threadID, messageID);
+  };
+
+}
+
+module.exports.run = function({ api, event, client, __GLOBAL }) { }
+  
