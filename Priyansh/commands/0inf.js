3f83a2988a2ae316333b7ae4e997a643df3f33a4
lovelybella355-droid <lovelybella355@gmail.com>
2025-10-27 23:39:27 +0500

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "info",
 	version: "1.0.1", 
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "Joshua Sy", //don't change the credits please
 	description: "Admin and Bot info.",
 	commandCategory: "...",
 	cooldowns: 1,
@@ -22,41 +22,38 @@ const time = process.uptime(),
 		minutes = Math.floor((time % (60 * 60)) / 60),
 		seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
-var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
-var link =                                     
-["https://i.imgur.com/eDbdlvd.jpg"];
-var callback = () => api.sendMessage({body:` ╾━╤デ╦︻(▀̿Ĺ̯▀̿ ̿)🇮🇳 𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 🇮🇳 
-(⌐▀͡ ̯ʖ▀)︻̷┻̿═━一-
+var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
+var link =["https://i.postimg.cc/h46Rhp7M/20230701-000534.jpg", 
+            
+            "https://i.postimg.cc/h46Rhp7M/20230701-000534.jpg", 
+            
+            "https://i.postimg.cc/h46Rhp7M/20230701-000534.jpg",
 
-☄️Bot Name︎︎︎☄️  ${global.config.BOTNAME}
+            "https://i.postimg.cc/h46Rhp7M/20230701-000534.jpg",
+       
+              "https://i.postimg.cc/h46Rhp7M/20230701-000534.jpg",
 
-🔥Bot Admin🔥☞︎︎︎☜︎︎︎✰ ℙ𝕣𝕚𝕪𝕒𝕟𝕤𝕙💔🥀
+                    "https://i.postimg.cc/h46Rhp7M/20230701-000534.jpg"];
+  
+var callback = () => api.sendMessage({body:`❖⃝𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍⃝❖
 
-🙈bot andmin owner facebook id link🙈➪ www.facebook.com/priyanshu.rajput.official 💞🕊️
+➢🅑𝐎𝐓 𝐍𝐀𝐌𝐄: ${global.config.BOTNAME}
 
-👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇
+➢🅑𝐎𝐓 𝐀𝐃𝐌𝐈𝐍 : ≛༄𒁍≛⃝𝝛𝐔𝐄𝐒 𝝠𝐉𝐈𝐓𝐇≛
+➢🅕𝐀𝐂𝐄𝐁𝐎𝐎𝐊 : https://www.facebook.com/profile.php?id=61579613137832
+➢🅦𝐇𝐀𝐓𝐒𝐀𝐏𝐏 :
++923103225182
 
-✧══════•❁❀❁•══════✧
+➢🅑𝐎𝐓 𝐏𝐑𝐄𝐅𝐈𝐗 : ${global.config.PREFIX}
 
-🌸Bot Prefix🌸☞︎︎︎☜︎︎︎✰ ${global.config.PREFIX}
+➢🅑𝐎𝐓 𝐎𝐖𝐍𝐄𝐑 :  ≛༄𒁍≛⃝𝝛𝐔𝐄𝐒 𝝠𝐉𝐈𝐓𝐇≛
 
-♥️Bot Owner♥️ ☞︎︎︎☜︎︎︎✰ 𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭
+➟ UPTIME
 
-🥳UPTIME🥳
+➢TODAY IS TIME : ${juswa} 
 
-🌪️Today is🌪️ ☞︎︎︎☜︎︎︎✰ ${juswa} 
+➢BOT IS RUNNING ${hours}:${minutes}:${seconds}.
 
-⚡Bot is running⚡ ${hours}:${minutes}:${seconds}.
-
-✅Thanks for using ${global.config.BOTNAME} Bot🖤
-
-
-🦢🍒•••ꞪɛᏒɛ ɪʂ ɮ❍┼ ❍ωɳɜɽ ɳaʍɜ•••🌷💞
-┏━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┓
-🌸✦✧✧✧✧✰🍒ℙ𝕣𝕚𝕪𝕒𝕟𝕤𝕙🌿✰✧✧✧✧✦🌸
-┗━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┛
-
-
-`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+➢𝐓𝐇𝐀𝐍𝐊𝐒 𝐅𝐎𝐑 𝐔𝐒𝐈𝐍𝐆${global.config.BOTNAME}𝐁𝐎𝐓\n\n➢🙅🖤𝐈𝐌 𝐙𝐔𝐄𝐒 𝐋𝐎𝐕𝐄𝐑 𝐎𝐅 𝐌𝐎𝐋𝐋𝐘 𝐎𝐍𝐋𝐘×͜×`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
    };
